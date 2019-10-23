5ec40a386036e200a16e4e450490e8bf8a90fb4d
wade <johnny12150@gmail.com>
2019-10-23 11:15:05 +0800

    compare np version results

--- HW1/HW1_0853410.py
+++ HW1/HW1_0853410.py
@@ -1,8 +1,11 @@
 import numpy as np
 from itertools import combinations_with_replacement
 
-feature = np.genfromtxt('./HW1/dataset_X.csv', delimiter=',')[:, 1:]
-label = np.genfromtxt('./HW1/dataset_T.csv', delimiter=',')[:, -1]
+feature = np.genfromtxt('./dataset_X.csv', delimiter=',')[:, 1:]
+label = np.genfromtxt('./dataset_T.csv', delimiter=',')[:, -1]
+m2 = np.load('./m2.npy')
+ww2 = np.load('./w2.npy')  # np 1.15
+w2_16 = np.load('./w2_16.npy')  # np 1.16
 
 
 def phi(X, M):
@@ -19,7 +22,6 @@ def phi(X, M):
     # 建立Phi matrix, bias內建(M=0)
     phi_matrix = np.ones((len(X), len(phi_list)+1))
     x_num = np.asarray(x_num)
-    print(x_num)
 
     # m筆data/ row
     for m in range(X.shape[0]):
@@ -29,22 +31,24 @@ def phi(X, M):
             K = np.sort(np.argwhere(n+1 >= x_num))[-1]+1
             for k in range(K[0]):
                 sum = sum * feature[m][phi_list[n][k - 1]]
-            # for k in range(1, M+1):
-            #     if n <= x_num[k]:
-            #         if k > len(phi_list[n]):
-            #             break
-            #         sum = sum*feature[m][phi_list[n][k-1]]
             # 第一col是bias不動
             phi_matrix[m, n+1] = sum
     return phi_matrix
 
 
+# 計算不同order的PHI matrix
 PHI = phi(feature, 1)
 PHI2 = phi(feature, 2)
 
 
 # Close form solution
 def get_close_form(X, Y):
+    """
+    透過close form算出weight
+    :param X: data的feature經過PHI轉換
+    :param Y: data的target/ label
+    :return:
+    """
     # first = np.dot(X.T, X)
     # second = np.dot(X.T, Y)
     
@@ -52,6 +56,26 @@ def get_close_form(X, Y):
     # return np.dot(np.linalg.inv(first), second)
 
 
-# M=1 時的close form weight
+# M=1 時的close form weight, 取前877筆當train
 w1 = get_close_form(phi(feature[:877], 1), label[:877])
 w2 = get_close_form(phi(feature[:877], 2), label[:877])
+# np.save('w2_16.npy', w2)
+np.save('w2_17.npy', w2)
+print(np.array_equal(w2_16, w2))  # np 1.16跟1.17算起來同
+print(np.array_equal(ww2, w2))  # np 1.15跟1.16算起來不同
+
+
+def close_form_l2(X, Y, lamda):
+    """
+    透過close form加上L2 norm算出weight
+    :param X: data的feature經過PHI轉換
+    :param Y: data的target/ label
+    :param lamda: L2的係數
+    :return:
+    """
+    tmp = np.linalg.inv(X.T.dot(X) + lamda * np.identity(X.shape[1])).dot(X.T)
+    return tmp.dot(Y)
+
+
+w1_l2 = close_form_l2(phi(feature[:877], 1), label[:877], 0.001)
+w2_l2 = close_form_l2(phi(feature[:877], 2), label[:877], 0.001)
