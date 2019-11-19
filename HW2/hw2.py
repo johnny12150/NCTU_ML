ccb4611850e6c41289eb33a1b9a9270a696a80b5
wade <johnny12150@gmail.com>
2019-11-19 10:32:32 +0800

    add pca for problem 3

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import Counter
+from collections import Counter
 # 第一題
 
 #%%
@@ -21,11 +21,6 @@ def PCA_np(features, n=2):
     print(eigenvector[:][:1].shape, C.shape)
     print(eigenvector[:][:1], eigenvector[:1], eigenvector)
     return eigenvector[:n].T.dot(C.T)
-    # if len(eigenvector[:n]) == len(features[0]):
-    #     # project data
-    #     return eigenvector[:].T.dot(C.T)
-    # else:
-    #     return np.matrix(mat.T)
 
 
 # 零均值化
@@ -41,16 +36,16 @@ def pca2(dataMat, n):
     covMat = np.cov(newData, rowvar=0)  # 求协方差矩阵,return ndarray；若rowvar非0，一列代表一个样本，为0，一行代表一个样本
 
     eigVals, eigVects = np.linalg.eig(np.mat(covMat))  # 求特征值和特征向量,特征向量是按列放的，即一列代表一个特征向量
-    eigValIndice = np.argsort(eigVals)  # 对特征值从小到大排序
-    n_eigValIndice = eigValIndice[-1:-(n + 1):-1]  # 最大的n个特征值的下标
-    n_eigVect = eigVects[:, n_eigValIndice]  # 最大的n个特征值对应的特征向量
-    lowDDataMat = newData * n_eigVect  # 低维特征空间的数据
-    reconMat = (lowDDataMat * n_eigVect.T) + meanVal  # 重构数据
+    eigValIndice = np.argsort(eigVals)  # sort eigenvalue
+    n_eigValIndice = eigValIndice[-1:-(n + 1):-1]  # select top n value
+    n_eigVect = eigVects[:, n_eigValIndice]  # select largest n eigenvector
+    lowDDataMat = newData * n_eigVect  # transform to low dim data
+    reconMat = (lowDDataMat * n_eigVect.T) + meanVal  # reconstruct to original data
     return lowDDataMat
     # return lowDDataMat, reconMat
 
 A = np.array([[1, 2], [3, 4], [5, 6]])
-PCA_np(A, 1).T # same as sklearn
+PCA_np(A, 2).T # same as sklearn
 pca2(A, 2)
 
 from sklearn.decomposition import PCA
@@ -97,9 +92,7 @@ for col in pokemon:
 pokemon_ohe = pokemon.copy()
 uniques = []
 for col in categorical_col:
-    # feature有哪些種類
-    # print(pokemon[col].value_counts().index.astype('str').tolist())
-    # 做one hot
+    # 用factorize做one hot
     pokemon_ohe[col], uni = pokemon[col].factorize()
     uniques.append(uni.tolist())
 
@@ -107,9 +100,9 @@ pokemon_norm = pokemon_ohe.copy()
 # normalize
 pokemon_norm = (pokemon_norm - pokemon_norm.mean()) / pokemon_norm.std()
 
-pokemon_np = pokemon_ohe.values
-# np style
-pokemon_np = (pokemon_np - np.mean(pokemon_np, 0)) / np.std(pokemon_np, 0)
+# pokemon_np = pokemon_ohe.values
+# # np style
+# pokemon_np = (pokemon_np - np.mean(pokemon_np, 0)) / np.std(pokemon_np, 0)
 
 target = pokemon_norm['Type 1'].copy()
 pokemon_norm = pokemon_norm.drop(['Type 1'], axis=1)
@@ -120,6 +113,24 @@ for k in range(1, 11):
     for j in range(38):
         j = 120+j
         predictions.append(knn(pokemon_norm.iloc[j, :], pokemon_norm[:120], target[:120], k))
-    # 計算Acc
+    # 1. 計算Acc
     acc.append(np.count_nonzero(predictions == target[120:]) / len(target[120:]))
 
+plot_knn(acc, range(1, 11))
+
+# 2. PCA到7, 6, 5維
+dim = [7, 6, 5]
+for d in dim:
+    acc = []
+    # convert np matrix to array
+    pca_data = np.asarray(pca2(pokemon_norm.values, d))
+    for k in range(1, 11):
+        predictions = []
+        #  compare each test sample with 120 training samples
+        for j in range(38):
+            j = 120 + j
+            predictions.append(knn(pca_data[j], pca_data[:120], target[:120], k))
+        # 計算Acc
+        acc.append(np.count_nonzero(predictions == target[120:]) / len(target[120:]))
+
+    plot_knn(acc, range(1, 11))
